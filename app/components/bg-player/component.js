84c04675c02a69e34b105ad5f686e81c3832c6cc
kmita <kmita69@gmail.com>
2016-04-25 16:18:47 +0200

    Form WIP

--- app/components/bg-player/component.js
+++ app/components/bg-player/component.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  didInsertElement() {
+  classNames: ['video-player'],
+  didRender() {
     this._super(...arguments);
+    setTimeout(()=>{
+      this.$().addClass("video-player-is-in");
+    }, 500);
   }
 });
