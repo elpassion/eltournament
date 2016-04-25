799cacbb16876b3fa069f6e055b1eb145143e835
Rafał Lisowski <lisukorin@gmail.com>
2016-04-25 13:18:09 +0200

    fake api

--- app/router.js
+++ app/router.js
@@ -7,6 +7,9 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('about-us');
+  this.route('sessions', function() {
+    this.route('new');
+  });
 });
 
 export default Router;
