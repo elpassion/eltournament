1a83c0e9a98d8097944df78f0f92d027265ab442
kmita <kmita69@gmail.com>
2016-04-26 11:34:56 +0200

    Add new tournament form

---
+++ tests/unit/helpers/sane-index-test.js
@@ -0,0 +1,10 @@
+import { saneIndex } from 'eltournament/helpers/sane-index';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | sane index');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = saneIndex([42]);
+  assert.ok(result);
+});
