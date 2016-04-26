36cf4b4a8d343f8503510610b55b538978d2e46a
Rafał Lisowski <lisukorin@gmail.com>
2016-04-26 15:49:40 +0200

    session

--- app/components/top-bar/component.js
+++ app/components/top-bar/component.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sessionPath: Ember.computed('session', function(){
+    return window.location.origin + '/' + this.get('session.name');
+  })
 });
--- app/sessions/show/route.js
+++ app/sessions/show/route.js
@@ -2,21 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params){
-    // return this.store.findRecord('tournament', params.id);
-    let _that = this;
-    let s = this.store.createRecord('tournament', {name: 'asd'});
-    s.get('players').addObject(
-      _that.store.createRecord('player', {name: 'John', id:98324792387, points:'987'})
-    );
-    s.get('players').addObject(
-      _that.store.createRecord('player', {name: 'John', id:325324, points:'987'})
-    );
-    s.get('players').addObject(
-      _that.store.createRecord('player', {name: 'John', id:962436, points:'987'})
-    );
-    s.get('players').addObject(
-      _that.store.createRecord('player', {name: 'John', id:147746254, points:'987'})
-    );
-    return s;
+    return this.store.findRecord('tournament', params.id);
+    // let _that = this;
+    // let s = this.store.createRecord('tournament', {name: 'asd'});
+    // s.get('players').addObject(
+    //   _that.store.createRecord('player', {name: 'John', id:98324792387, points:'987'})
+    // );
+    // s.get('players').addObject(
+    //   _that.store.createRecord('player', {name: 'John', id:325324, points:'987'})
+    // );
+    // s.get('players').addObject(
+    //   _that.store.createRecord('player', {name: 'John', id:962436, points:'987'})
+    // );
+    // s.get('players').addObject(
+    //   _that.store.createRecord('player', {name: 'John', id:147746254, points:'987'})
+    // );
+    // return s;
   }
 });
